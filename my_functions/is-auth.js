d43508d30c4f0a2f2c2fc0f5291e8a4e18f29514
yehuda buzaglo <yehudabu@rafael.co.il>
2023-08-08 12:22:03 +0300

    stuyff

--- my_functions/is-auth.js
+++ my_functions/is-auth.js
@@ -1,6 +1,6 @@
 exports.handler = async (event, context) => {
   return {
     statusCode: 200,
-    body: JSON.stringify({ message: "Hello from Netlify Function!" }),
+    body: JSON.stringify({ message: event }),
   };
 };
