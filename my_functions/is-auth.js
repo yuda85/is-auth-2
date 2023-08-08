3ec69d7fd6762e10f9f3f945d7dcb289666626d6
yehuda buzaglo <yehudabu@rafael.co.il>
2023-08-08 16:04:24 +0300

    stuff

--- my_functions/is-auth.js
+++ my_functions/is-auth.js
@@ -1,6 +1,22 @@
 exports.handler = async (event, context) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify({ c: context }),
-  };
+  try {
+    const { token } = event.queryStringParameters;
+
+    if (!token) {
+      return {
+        statusCode: 400,
+        body: JSON.stringify({ error: "Token is missing" }),
+      };
+    }
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ token }),
+    };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: "Internal Server Error" }),
+    };
+  }
 };
