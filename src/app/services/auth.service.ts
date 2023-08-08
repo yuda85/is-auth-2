04f4ac3bd2221dfc7716a8feef0550911f64a1cf
yehuda buzaglo <yehudabu@rafael.co.il>
2023-08-08 16:12:23 +0300

    stuff

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -14,8 +14,13 @@ export class AuthService {
   isAuth(token: string) {
     return this.http
       .get<any>(`${this.apiUrl}/is-auth?token=${token}`)
-      .subscribe((data) => {
-        console.log(data);
-      });
+      .subscribe(
+        (data) => {
+          console.log(data);
+        },
+        (err) => {
+          window.location.replace('https://is-auth-main.netlify.app/login');
+        }
+      );
   }
 }
